158e08bfc400fd637f0551d31450a572c1829887
Shrikesh Shetty <ffgameplay39@gmail.com>
2025-04-13 12:01:58 +0530

    Delete database directory

--- database/shrikesh_authentication.sql
+++
@@ -1,8 +0,0 @@
-CREATE DATABASE shrikesh_authentication;
-USE shrikesh_authentication;
-
-CREATE TABLE users (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    username VARCHAR(50) NOT NULL UNIQUE,
-    password VARCHAR(255) NOT NULL
-);
